c0c6cad15ca20a6a62f740ba6d9366bff5927f06
HasaraDananjaya <hasaradananjaya003@gmail.com>
2021-11-21 10:59:10 +0530

    adjustments on some pages

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Typography } from '@material-ui/core';
+import React from 'react';
+import Layout from '../components/Layout';
+import useStyles from '../utils/styles';
 
 export default function About() {
-    return (
-        <div>
-            About
-        </div>
-    )
+  const classes = useStyles();
+  return (
+    <Layout title="Shipping">
+      <form className={classes.form}>
+        <Typography component="h1" variant="h1">
+          Details about the company Here
+        </Typography>
+      </form>
+    </Layout>
+  );
 }
--- pages/shipping.js
+++ pages/shipping.js
@@ -1,14 +1,26 @@
 import React, { useContext } from 'react';
 import { useRouter } from 'next/router';
 import { Store } from '../utils/Store';
+import Layout from '../components/Layout';
+import useStyles from '../utils/styles';
+import { Typography } from '@material-ui/core';
 
 export default function Shipping() {
   const router = useRouter();
   const { state, dispatch } = useContext(Store);
   const { userInfo } = state;
+  const classes = useStyles();
 
   if (!userInfo) {
     router.push('/login?redirect=/shipping');
   }
-  return <div>Shipping</div>;
+  return (
+    <Layout title="Shipping">
+      <form className={classes.form}>
+        <Typography component="h1" variant="h1">
+          Delivery Details Here
+        </Typography>
+      </form>
+    </Layout>
+  );
 }
